21a4ef6dc2a19698508431133fd71a7e68d4d4b2
王炜 <wangwei7426@gmail.com>
2021-03-13 20:44:39 +0800

    first commit

---
+++ luxun/src/WeeklyReport1.java
@@ -0,0 +1,7 @@
+/**
+ * @Author: wangdawei
+ * @Description:
+ * @Date: 2021/3/13 20:19
+ */
+public class WeeklyReport1 {
+}
---
+++ luxun/src/WeeklyReport2.java
@@ -0,0 +1,10 @@
+/**
+ * @Author: wangdawei
+ * @Description:
+ * @Date: 2021/3/13 20:20
+ */
+public class WeeklyReport2 {
+    public static void main(String[] args){
+        System.out.println("Hello");
+    }
+}
