389525d379822748c8858f95427768313aaeb176
913859464 <913859464@qq.com>
2021-03-16 11:08:24 +0800

    111

--- yanshibin/src/secondweek/weather/Application.java
+++ yanshibin/src/secondweek/weather/Application.java
@@ -1,30 +1,51 @@
 package secondweek.weather;
 
-import com.imooc.weather.HourWeather;
-import com.imooc.weather.WeatherUtils;
-import com.imooc.weather.impl.WeatherUtilsImpl;
 
-import java.util.List;
+import java.text.SimpleDateFormat;
+import java.util.Calendar;
+import java.util.Date;
 import java.util.Scanner;
-
-import java.net.HttpURLConnection;
-import java.net.URL;
 
 public class Application {
     public static void main(String[] args) {
         System.out.print("天气预报查询：\n输入1：查询未来24小时天气预报\n" +
-                "输入2：查询未来3天天气预报\n输入3：查询未来7天天气预报\n");
+                "输入2-7：查询未来n天天气预报(2≤n≤7)\n");
         System.out.print("请输入你的选择：");
         Scanner scanner = new Scanner(System.in);
         int i = scanner.nextInt();
-        System.out.println("用户输入："+i);
         if (i==1){
             System.out.print("请输入城市名称查询未来24小时天气预报:");
             String city = scanner.next();
             System.out.println(city+"未来24小时天气预报：");
-            WeatherUtils weatherUtils = new WeatherUtilsImpl();
-            List<HourWeather> weatherlist = weatherUtils.w24h("89b3d33757314ff5a82dd5d72fb4673a", city);
-            System.out.println(weatherlist);
+            baseWeather weather = new baseWeatherImpl();
+            String result = weather.query24h("89b3d33757314ff5a82dd5d72fb4673a",city);
+            System.out.println(result);
+        }else if(i>=2 && i<=7){
+            System.out.print("请输入城市名称查询未来"+i+"天天气预报:");
+            String city = scanner.next();
+            System.out.println(city+"未来"+i+"天天气预报：");
+            baseWeather weather = new baseWeatherImpl();
+
+            int cd = 0;
+            SimpleDateFormat df = new SimpleDateFormat("yyyyMMdd");//设置日期格式
+            String currentDate = df.format(new Date());// new Date()为获取当前系统时
+            try {
+                cd = Integer.parseInt(currentDate);
+            } catch (NumberFormatException e) {
+                e.printStackTrace();
+            }
+            for (int j=1;j<=i;j++){
+                Calendar calendar2 = Calendar.getInstance();
+                SimpleDateFormat sdf2 = new SimpleDateFormat("yyyyMMdd");
+                calendar2.add(Calendar.DATE, j);
+                String j_days_after = sdf2.format(calendar2.getTime());
+                System.out.println(j_days_after);
+
+                String result = weather.query7d("89b3d33757314ff5a82dd5d72fb4673a",city,j_days_after);
+                System.out.println(result);
+            }
+        }else {
+            System.out.print("暂不支持你输入的查询条件！");
         }
     }
 }
