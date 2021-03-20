2eac8a758ad5b4e916505b40e413b2089dad11cd
913859464 <913859464@qq.com>
2021-03-20 15:55:43 +0800

    111

---
+++ yanshibin/src/thirdweek/section2_1.java
@@ -0,0 +1,13 @@
+package thirdweek;
+
+public class section2_1 {
+    public static void main(String[] args) {
+        //定义字符串
+        String hobby = new String("爱慕课");
+        String url = new String("www.imooc.com");
+
+        //输出字符串
+        System.out.println("hobby:" + hobby);
+        System.out.println("url:" + url);
+    }
+}
